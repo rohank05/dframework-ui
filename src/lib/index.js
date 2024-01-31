e4b7bff6646cdd6c28e96ce8b396e0f07b272b24
Collins87mbathi <perezcollins87@gmail.com>
2024-01-31 19:04:21 +0300

    fix:fixed the links as expected

--- src/lib/index.js
+++ src/lib/index.js
@@ -6,5 +6,15 @@ import { UiModel } from './components/Grid/ui-models';
 import HelpModal from './components/HelpModal';
 import PageTitle from './components/PageTitle';
 import MuiTypography from './components/Typography';
-import { StateProvider, useStateContext, useRouter, RouterProvider } from './components/useRouter/StateProvider'
-export { SnackbarProvider, SnackbarContext, useSnackbar, DialogComponent, UiModel, GridBase, RouterProvider, MuiTypography, PageTitle, StateProvider, useStateContext, HelpModal, useRouter };
+import useLocalization, { locales } from './components/mui/locale/localization';
+import { daDKGrid } from './components/mui/locale/data-grid/daDK';
+import { deDEGrid } from './components/mui/locale/data-grid/deDE';
+import { elGRGrid } from './components/mui/locale/data-grid/elGR';
+import { esESGrid } from './components/mui/locale/data-grid/esES';
+import { frFRGrid } from './components/mui/locale/data-grid/frFR';
+import { itITGrid } from './components/mui/locale/data-grid/itIT';
+import ptPT from './components/mui/locale/data-grid/ptPT';
+import { trTRGrid } from './components/mui/locale/data-grid/trTR';
+import {StateProvider, useStateContext, useRouter, RouterProvider} from './components/useRouter/StateProvider';
+import useMobile from './components/useMobile';
+export { SnackbarProvider, SnackbarContext, useSnackbar, DialogComponent, UiModel, GridBase, RouterProvider, MuiTypography, PageTitle, locales, useLocalization, daDKGrid, deDEGrid, elGRGrid, esESGrid, frFRGrid, itITGrid, ptPT, trTRGrid,   StateProvider, useStateContext,HelpModal,useMobile, useRouter};
