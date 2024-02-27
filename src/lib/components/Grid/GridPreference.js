5905665d8a409b64b82e5aff0ade2df800f16dfa
Collins87mbathi <perezcollins87@gmail.com>
2024-02-27 09:54:07 +0300

    Fix:fixed delete preference

--- src/lib/components/Grid/GridPreference.js
+++ src/lib/components/Grid/GridPreference.js
@@ -180,7 +180,7 @@ const GridPreferences = ({ preferenceName, gridRef, columns = [], setIsGridPrefe
                 Username,
                 prefId
             };
-            const response = await request({ url: apis.Preference, params, history, dispatch });
+            const response = await request({ url: preferenceApi, params, history, dispatch });
             userPreferenceCharts = response?.prefValue ? JSON.parse(response.prefValue) : null;
             coolrDefaultPreference = response?.prefValue ? response.prefName : '';
         }
