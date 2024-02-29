cfec44b1b45d528078af846ac425fb2d3b3b110f
Collins87mbathi <perezcollins87@gmail.com>
2024-02-29 11:23:43 +0300

    Fix:fixed the issue

--- dist/components/SnackBar/index.js
+++ dist/components/SnackBar/index.js
@@ -16,6 +16,8 @@ function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return 
 function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != typeof e && "function" != typeof e) return { default: e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && Object.prototype.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n.default = e, t && t.set(e, n), n; }
 function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 const SnackbarContext = exports.SnackbarContext = /*#__PURE__*/(0, _react.createContext)(null);
+const vertical = 'bottom';
+const horizontal = 'center';
 const Alert = /*#__PURE__*/_react.default.forwardRef(function Alert(props, ref) {
   return /*#__PURE__*/_react.default.createElement(_Alert.default, _extends({
     elevation: 6,
@@ -65,7 +67,11 @@ const SnackbarProvider = _ref => {
   }, children), /*#__PURE__*/_react.default.createElement(_Snackbar.default, {
     open: open,
     autoHideDuration: 6000,
-    onClose: handleClose
+    onClose: handleClose,
+    anchorOrigin: {
+      vertical,
+      horizontal
+    }
   }, /*#__PURE__*/_react.default.createElement(Alert, {
     severity: severity
   }, message)));
