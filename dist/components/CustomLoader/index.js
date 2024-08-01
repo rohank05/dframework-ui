5b3f39653843c1c63c45418d26d138f21e9e711b
Segun <jidonu2@gmail.com>
2024-08-01 11:27:43 +0000

    add react import on customLoader

--- dist/components/CustomLoader/index.js
+++ dist/components/CustomLoader/index.js
@@ -4,7 +4,9 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
 var _material = require("@mui/material");
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
 const CustomLoader = () => {
   const gridStyle = {
     width: "60vw",
@@ -16,12 +18,12 @@ const CustomLoader = () => {
     top: "50%",
     transform: "translateX(-50%) translateY(-50%)"
   };
-  return /*#__PURE__*/React.createElement(_material.Grid, {
+  return /*#__PURE__*/_react.default.createElement(_material.Grid, {
     container: true,
     justifyContent: "center",
     alignItems: "center",
     style: gridStyle
-  }, /*#__PURE__*/React.createElement(_material.Typography, {
+  }, /*#__PURE__*/_react.default.createElement(_material.Typography, {
     variant: "body2",
     id: "footer",
     color: "text.secondary",
--- src/lib/components/CustomLoader/index.mjs
+++ src/lib/components/CustomLoader/index.mjs
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Grid, Typography } from "@mui/material";
 
 const CustomLoader = () => {
