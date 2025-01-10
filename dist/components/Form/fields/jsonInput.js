f9bc269851a2ed831d0e0e11c6ec437776bd4196
suyash pratap singh <suyash.singh@spraxa.com>
2025-01-10 17:02:26 +0530

    json input 1. input works good

--- dist/components/Form/fields/jsonInput.js
+++ dist/components/Form/fields/jsonInput.js
@@ -3,23 +3,21 @@
 require("core-js/modules/es.error.cause.js");
 require("core-js/modules/es.array.push.js");
 require("core-js/modules/es.weak-map.js");
+require("core-js/modules/esnext.iterator.constructor.js");
+require("core-js/modules/esnext.iterator.filter.js");
 require("core-js/modules/esnext.iterator.for-each.js");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-require("core-js/modules/es.array.includes.js");
 require("core-js/modules/es.json.stringify.js");
-require("core-js/modules/es.string.includes.js");
-require("core-js/modules/esnext.iterator.constructor.js");
-require("core-js/modules/esnext.iterator.filter.js");
 require("core-js/modules/esnext.iterator.map.js");
 require("core-js/modules/web.dom-collections.iterator.js");
 var React = _interopRequireWildcard(require("react"));
-var _material = require("@mui/material");
 var _FormControl = _interopRequireDefault(require("@mui/material/FormControl"));
-var _Autocomplete = _interopRequireDefault(require("@mui/material/Autocomplete"));
-var _TextField = _interopRequireDefault(require("@mui/material/TextField"));
+var _Input = _interopRequireDefault(require("@mui/material/Input"));
+var _Typography = _interopRequireDefault(require("@mui/material/Typography"));
+var _debounce = _interopRequireDefault(require("lodash/debounce"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
 function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
 function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != typeof e && "function" != typeof e) return { default: e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n.default = e, t && t.set(e, n), n; }
@@ -29,7 +27,6 @@ function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == typeof i ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != typeof t || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != typeof i) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 const Field = _ref => {
-  var _formik$values$field;
   let {
     column,
     field,
@@ -42,40 +39,64 @@ const Field = _ref => {
     fieldConfigs,
     mode
   } = _ref;
-  // const inputJSON = JSON.parse(formik.values[field]);
   const [state, setState] = React.useState({});
-  console.log(state);
-  let inputValue = ((_formik$values$field = formik.values[field]) === null || _formik$values$field === void 0 || (_formik$values$field = _formik$values$field.split(", ")) === null || _formik$values$field === void 0 ? void 0 : _formik$values$field.map(Number)) || [];
-  const options = lookups ? lookups[column === null || column === void 0 ? void 0 : column.lookup] : [];
-  let filteredCombos = (options === null || options === void 0 ? void 0 : options.filter(option => inputValue.includes(option.value))) || [];
-  let isDisabled;
-  if (mode !== 'copy') {
-    isDisabled = fieldConfigs === null || fieldConfigs === void 0 ? void 0 : fieldConfigs.disabled;
-  }
-  const handleAutoCompleteChange = (event, newValue) => {
-    formik === null || formik === void 0 || formik.setFieldValue(field, newValue ? newValue.map(val => val.value).join(', ') : '');
-  };
+  console.log(formik.values[field]);
+  // Initialize state based on formik values
   React.useEffect(() => {
     if (!formik.values[field]) return;
     const inputJSON = JSON.parse(formik.values[field]);
     setState(inputJSON);
   }, [formik.values[field]]);
+
+  // Debounced handler for updating formik field
+  const handleDebouncedChange = React.useMemo(() => (0, _debounce.default)(newState => {
+    formik.setFieldValue(field, JSON.stringify(newState));
+  }, 300), [formik, field]);
+
+  // Handle input changes
+  const handleChange = (key, value) => {
+    const updatedState = _objectSpread(_objectSpread({}, state), {}, {
+      [key]: value
+    });
+    setState(updatedState);
+    handleDebouncedChange(updatedState);
+  };
+  React.useEffect(() => {
+    return () => {
+      handleDebouncedChange.cancel(); // Cleanup debounce on component unmount
+    };
+  }, [handleDebouncedChange]);
   return /*#__PURE__*/React.createElement(_FormControl.default, {
     fullWidth: true,
     key: field,
     variant: "standard",
-    error: formik.touched[field] && Boolean(formik.errors[field])
-  }, Object.keys(state).map((key, index) => /*#__PURE__*/React.createElement(_TextField.default, {
+    error: formik.touched[field] && Boolean(formik.errors[field]),
+    style: {
+      marginBottom: '1rem'
+    }
+  }, Object.keys(state).map(key => /*#__PURE__*/React.createElement("div", {
     key: key,
-    label: key,
+    style: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      marginBottom: '0.5rem'
+    }
+  }, /*#__PURE__*/React.createElement(_Typography.default, {
+    variant: "body1",
+    sx: {
+      width: "150px",
+      marginRight: 2
+    }
+  }, key, " :"), /*#__PURE__*/React.createElement(_Input.default, {
+    id: key,
     name: key,
     value: state[key],
-    onChange: e => {
-      setState(_objectSpread(_objectSpread({}, state), {}, {
-        [key]: e.target.value
-      }));
-      formik.setFieldValue(field, JSON.stringify(state));
+    onChange: e => handleChange(key, e.target.value),
+    fullWidth: true,
+    style: {
+      flex: 2
     }
-  })));
+  }))));
 };
 var _default = exports.default = Field;
